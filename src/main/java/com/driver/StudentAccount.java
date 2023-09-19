ce57680431cee45c6371b5a0a40aba6b0a638873
Rahul Kishne <kishnerahul123@gmail.com>
2023-09-19 19:39:27 +0530

    update5

--- src/main/java/com/driver/StudentAccount.java
+++ src/main/java/com/driver/StudentAccount.java
@@ -6,8 +6,6 @@ public class StudentAccount extends BankAccount{
 
     public String getInstitutionName() {
         return institutionName;
-
-
     }
 
     public void setInstitutionName(String institutionName) {
